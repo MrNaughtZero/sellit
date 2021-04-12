900e63b1dfd26b2dcef8ab5ca0796f9a5b54dd2e
MrNaughtZero <neil.naught@icloud.com>
2021-04-12 19:16:24 +0100

    regex created for IP validation

--- webapp/utils/regex.py
+++ webapp/utils/regex.py
@@ -3,6 +3,9 @@ import re
 def email_regex(email):
     return re.match('[^@]+@[^@]+\.[^@]+', email)
 
+def ip_regex(ip):
+    return re.match('^(([1-9]?\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}([1-9]?\d|1\d\d|2[0-5][0-5]|2[0-4]\d)$', ip)
+
 def check_if_upload_is_image(attachment):
     ''' check the type of upload '''
     return re.search("(?i)\.(jpg|png|gif|svg|jpeg)$", attachment)

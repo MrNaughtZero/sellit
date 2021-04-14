c3ffa05c2ab25d9022e91c78f6e2674777cd0469
MrNaughtZero <neil.naught@icloud.com>
2021-04-14 17:08:28 +0100

    payment_complete route updated to apply a order_hash inside the session after order is complete. // email sent with feedback link

--- webapp/routes/order.py
+++ webapp/routes/order.py
@@ -3,7 +3,7 @@ from webapp.models import Order, Coupon, User, Product, Attachment
 from webapp.payments.paypal import capture_paypal_order, get_paypal_order
 from webapp.utils.uploads import download_file
 from webapp.utils.tasks import leave_feedback
-from flask import Blueprint, request, redirect, url_for, flash, render_template, get_flashed_messages, abort, Response
+from flask import Blueprint, request, redirect, url_for, flash, render_template, get_flashed_messages, abort, Response, session
 from os import environ
 
 order_bp = Blueprint("order", __name__)
@@ -61,6 +61,8 @@ def payment_complete(order_id):
     
         if check_order['payment_status'] == 'COMPLETED':
             order.status = 'Completed'
+            order.order_hash = Order.set_order_hash()
+            session['order_hash'] = order.order_hash
 
         if not order.email:
             order.email = check_order['buyer_email']
@@ -68,7 +70,7 @@ def payment_complete(order_id):
         order.update()
 
         deliver_goods = Product().deliver_product(order.product_id, order_id, order.quantity, order.email)
-        leave_feedback.apply_async()
+        leave_feedback.apply_async(args=[order.email, User().fetch_user_supply_uuid(order.user).username, order_id])
             
     return redirect(url_for('order.track_order', order_id=order_id))
 

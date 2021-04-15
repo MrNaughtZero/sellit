dc6a559b3faf65d597531b5d8022298a8f8825e2
MrNaughtZero <neil.naught@icloud.com>
2021-04-15 02:11:47 +0100

    update order hash for feedback route removed. // leave_feedback paramters updated // submit_feedback route updated

--- webapp/routes/shop.py
+++ webapp/routes/shop.py
@@ -97,46 +97,26 @@ def check_coupon():
     else:
         return jsonify({'Success':'True', 'Discount':f'0.{check}'}), 200
 
-@shop_bp.route('/order/<string:order_id>/<string:email>/<string:order_hash>/feedback', methods=['GET'])
-def feedback_hash(order_id, email, order_hash):
-    order = Order().query.filter_by(id=order_id, email=email, order_hash=order_hash).first()
-    
-    if not order:
-        return abort(404)
-
-    session['order_hash'] = order.order_hash
-  
-    return redirect(url_for('shop.leave_feedback', order_id=order_id, username=User().fetch_user_supply_uuid(order.user).username))
-
-@shop_bp.route('/@<string:username>/order/<string:order_id>/feedback', methods=['GET'])
-def leave_feedback(username, order_id):
+@shop_bp.route('/@<string:username>/order/<string:order_id>/<string:order_hash>/feedback', methods=['GET'])
+def leave_feedback(username, order_id, order_hash):
     order = Order().fetch_order(order_id)
-
-    if not order:
+    
+    if (not order) or (order.order_hash != order_hash):
         return abort(404)
     
-    if (not session.get('order_hash')) or (session['order_hash'] != order.order_hash):
-        Order().update_order_hash(order_id, username)
-        
-        flash(['Please check your email. You can leave feedback by clicking the link in your email'])
-        return redirect(url_for('shop.shop', username=username))
+    return render_template('/shop/feedback.html', user=User().fetch_user_supply_username(username), form=FeedbackForm(), order_id=order_id, order_hash=order_hash)
 
-    return render_template('/shop/feedback.html', user=User().fetch_user_supply_username(username), form=FeedbackForm())
-
-@shop_bp.route('/feedback/submit', methods=['POST'])
-def submit_feedback():
+@shop_bp.route('/@<string:username>/order/<string:order_id>/<string:order_hash>/feedback/submit', methods=['POST'])
+def submit_feedback(username, order_id, order_hash):
     form = FeedbackForm()
 
     if not form.validate_on_submit():
         flash(list(form.errors.values())[0])
         return redirect(request.referrer)
-    
-    if not session.get('order_hash'):
-        return abort(404)
 
-    order = Order().query.filter_by(order_hash=session['order_hash']).first()
+    order = Order().fetch_order(order_id)
 
-    if not order:
+    if (not order) or (order.order_hash != order_hash):
         return abort(404)
 
     feedback = Feedback().fetch_feedback(order.id)
